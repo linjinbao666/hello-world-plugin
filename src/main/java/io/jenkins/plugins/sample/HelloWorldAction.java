8e616680dc5720a1c519419c519dd2e6be1dbf0a
linjinbao666 <linjinbao666@gmail.com>
2025-09-22 05:42:35 +0000

    init

---
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -0,0 +1,21 @@
+package io.jenkins.plugins.sample;
+
+import hudson.model.Action;
+
+public class HelloWorldAction implements Action {
+
+    @Override
+    public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        return null;
+    }
+}
